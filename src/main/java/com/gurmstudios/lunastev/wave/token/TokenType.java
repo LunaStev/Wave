85664d07ec2cb51699c3213f37df1c947bbddce2
Quin0019 <youngjae681@gmail.com>
2024-05-30 20:30:16 +0900

    토큰 추가

--- src/main/java/com/gurmstudios/lunastev/wave/token/TokenType.java
+++ src/main/java/com/gurmstudios/lunastev/wave/token/TokenType.java
@@ -2,19 +2,32 @@ package com.gurmstudios.lunastev.wave.token;
 
 public class TokenType {
     public enum Token {
-        FUN,    // 함수
-        VAR,    // 문자 변수
-        COUNT,  // 숫자 변수
+        FUN,            // 함수
+        VAR,            // 문자 변수
+        COUNT,          // 숫자 변수
         IF,
         FOR,
         WHILE,
         ELSE,
 
-        PLUS,   // '+'
-        MINUS,  // '-'
-        TIMES,  // '*'
-        SLASH,  // '/'
+        SEMI,           // ';'
+        COLON,          // ':'
+
+        L_BRA,       // '('
+        R_BRA,       // ')'
+        L_C_BRA,     // '{'
+        R_C_BRA,     // '}'
+        L_S_BRA,     // '['
+        R_S_BRA,     // ']'
+        L_A_BRA,     // '<'
+        R_A_BRA,     // '>'
+
+        PLUS,           // '+'
+        MINUS,          // '-'
+        TIMES,          // '*'
+        SLASH,          // '/'
 
         IMPORT,
+        RETURN,
     }
 }
