9672a889a0e49b75e0ea50d12bfa0434be510d5c
Quin0019 <youngjae681@gmail.com>
2024-05-30 22:42:18 +0900

    토큰 타입 추가

--- src/main/java/lunastev/wave/token/TokenType.java
+++ src/main/java/lunastev/wave/token/TokenType.java
@@ -1,33 +1,37 @@
 package lunastev.wave.token;
 
-public class TokenType {
-    public enum Token {
-        FUN,            // 함수
-        VAR,            // 문자 변수
-        COUNT,          // 숫자 변수
-        IF,
-        FOR,
-        WHILE,
-        ELSE,
+public enum TokenType {
+    FUN,            // 함수
+    VAR,            // 문자 변수
+    COUNT,          // 숫자 변수
+    IF,
+    FOR,
+    WHILE,
+    ELSE,
 
-        SEMI,           // ';'
-        COLON,          // ':'
+    TRUE,
+    FALSE,
+    NULL,
 
-        L_BRA,       // '('
-        R_BRA,       // ')'
-        L_C_BRA,     // '{'
-        R_C_BRA,     // '}'
-        L_S_BRA,     // '['
-        R_S_BRA,     // ']'
-        L_A_BRA,     // '<'
-        R_A_BRA,     // '>'
+    COMMA,
+    SEMI,           // ';'
+    COLON,          // ':'
 
-        PLUS,           // '+'
-        MINUS,          // '-'
-        TIMES,          // '*'
-        SLASH,          // '/'
+    L_BRA,       // '('
+    R_BRA,       // ')'
+    L_C_BRA,     // '{'
+    R_C_BRA,     // '}'
+    L_S_BRA,     // '['
+    R_S_BRA,     // ']'
+    L_A_BRA,     // '<'
+    R_A_BRA,     // '>'
 
-        IMPORT,
-        RETURN,
-    }
+    PLUS,           // '+'
+    MINUS,          // '-'
+    TIMES,          // '*'
+    SLASH,          // '/'
+
+    IMPORT,
+    RETURN,
 }
+
