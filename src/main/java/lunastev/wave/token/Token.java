b3775586be0a43a0219efab2b841f4b8a1d257ab
Quin0019 <youngjae681@gmail.com>
2024-05-30 22:42:01 +0900

    토큰

--- src/main/java/lunastev/wave/token/Token.java
+++ src/main/java/lunastev/wave/token/Token.java
@@ -1,4 +1,19 @@
 package lunastev.wave.token;
 
 public class Token {
+
+    private int beginIndex;
+
+    private int endIndex;
+
+    private TokenType tokenType;
+
+    private String tokenString;
+
+    public Token(int beginIndex, int endIndex, String tokenString, TokenType tokenType) {
+        this.beginIndex = beginIndex;
+        this.endIndex = endIndex;
+        this.tokenType = tokenType;
+        this.tokenString = tokenString;
+    }
 }
