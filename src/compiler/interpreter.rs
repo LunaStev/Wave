70a40c2e6597b69c50f506cc2fc4c3b9bf0c7670
LunaStev <quingames3390@gmail.com>
2024-06-20 14:17:49 +0900

    컴파일러가 아닌 인터프리터로 먼저 개발

--- src/compiler/interpreter.rs
+++ src/compiler/interpreter.rs
@@ -9,5 +9,44 @@ impl Compile for Interpreter {
     fn from_ast(ast: Vec<Node>) -> Self::Output {
         let mut ret = 0i32;
         let evaluator = Eval::new();
+        for node in ast {
+            ret += evaluator.dval(&node);
+        } Ok(ret)
     }
+}
+
+struct Eval;
+
+impl Eval {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn eval(&self, node: &Node) -> i32 {
+        match node {
+            Node::Int(n) => *n,
+            Node::UnaryExpr {op, child } => {
+                let child = self.eval(child);
+                match op {
+                    Operator::PLUS => child,
+                    Operator::MINUS => -child,
+                }
+            }
+            Node::BinaryExpr { op, lhs, rhs } => {
+                let lhs_ret = self.eval(lhs);
+                let rhs_ret = self.eval(rhs);
+
+                match op {
+                    Operator::PLUS => lhs_ret + rhs_ret,
+                    Operator::MINUS => lhs_ret - rhs_ret,
+                }
+            }
+
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
 }
