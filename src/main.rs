a60e1eafd6de70d1ee50232621d1a9a15841e1ad
LunaStev <quingames3390@gmail.com>
2024-06-20 14:18:02 +0900

    실행 파일

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use cfg_if::cfg_if;
 
+use wave::Compile;
+cfg_if! {
+    if #[cfg(feature = "default")] {
+        use wave::Jit as Engine;
+    }
+    else {
+        use wave::Compile::Interpreter as Engine;
+    }
+}
 
 fn main() {
-    println!("Hello World!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("No input file was provided");
+        std::process::exit(-1);
+    }
+
+    println!(
+        "{:?}",
+        Engine::from_source(&std::fs::read_to_string(&args[1]).unwrap()).unrap()
+    );
 }
