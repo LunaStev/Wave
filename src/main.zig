b36ed6905fc6470c032890109bd9ffd0b1cab53b
LunaStev <youngjae681@gmail.com>
2024-08-16 21:04:20 +0900

    기존 테스트에서 파서로 컴파일

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,20 @@
 const std = @import("std");
 const wave_lexer = @import("lexer.zig");
+const wave_parser = @import("parser.zig");
+
+// pub fn main() !void {
+//     var lexer = wave_lexer.Lexer.init("fun main() {    const a = 10 + 1; }");
+//
+//     var token = lexer.next_token();
+//     while (token.kind != wave_lexer.TokenType.ENDOFFILE) {
+//         std.debug.print("Token: {s} - {s}\n", .{ @tagName(token.kind), token.value });
+//         token = lexer.next_token();
+//     }
+// }
 
 pub fn main() !void {
-    var lexer = wave_lexer.Lexer.init("fun main() {    const a = 10 + 1; }");
+    const lexer = wave_lexer.Lexer.init("import iosys; fun hello() { hello(); var a = 0;} fun main() { var a = 1;}");
+    var parser = wave_parser.Parser.init(lexer);
 
-    var token = lexer.next_token();
-    while (token.kind != wave_lexer.TokenType.ENDOFFILE) {
-        std.debug.print("Token: {s} - {s}\n", .{ @tagName(token.kind), token.value });
-        token = lexer.next_token();
-    }
+    try parser.parse();
 }
