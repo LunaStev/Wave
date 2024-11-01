6bca5251abcd8f19436f9f12249513bd42c16709
LunaStev <youngjae681@gmail.com>
2024-11-01 16:13:24 +0900

    Write ast-related code

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,34 @@
-#[derive(Debug, PartialEq)]
-pub struct AST {
-    // ast
+#[derive(Debug)]
+pub enum Expr {
+    Number(i64),
+    Variable(String),
+    StringLiteral(String),
+    Identifier(String),
+    Binary(Box<Expr>, Operator, Box<Expr>),
 }
 
-impl<'a> AST {
-    //ast
+#[derive(Debug)]
+pub enum Statement {
+    VariableDeclaration(String, Box<Expr>, Box<Statement>),
+    FunctionDeclaration(String, Vec<String>, Box<Statement>),
+    IfStatement(Box<Expr>, Box<Statement>, Option<Box<Statement>>),
+    WhileStatement(Box<Expr>, Box<Statement>),
+    ImportStatement(String),
+    Print(Box<Expr>),
+    Empty,
+}
+
+#[derive(Debug)]
+pub enum Operator {
+    Plus,
+    Minus,
+    Star,
+    Div,
+    Assign,
+}
+
+#[derive(Debug)]
+pub struct Function {
+    pub name: String,
+    pub body: Vec<Statement>,
 }
