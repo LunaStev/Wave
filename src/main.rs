ecbce24782cd3935451b15a03aa915a94cc8e18a
LunaStev <youngjae681@gmail.com>
2024-12-18 16:47:37 +0900

    Source code routine

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,7 @@ use parser::Parser;
 
 fn main() {
     // Sample code to parse and run
-    let code_a = r#"
-    fun main() {
-        var a: i32 = 10;
-        println("World {}", a);
-    }
-    "#;
+    let code_a = "fun main() {    var a: i32 = 10;    println();    }";
 
     // Create a Lexer
     let mut lexer = Lexer::new(code_a);
