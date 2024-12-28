457d05ab41bc6bafa2901ab1b133c8ec1cfe57d2
LunaStev <youngjae681@gmail.com>
2024-12-28 16:29:26 +0900

    Node Is Very Not

--- src/node.rs
+++ src/node.rs
@@ -1,18 +1,24 @@
+use std::fs;
 use crate::ast::AST;
 use crate::lexer::Lexer;
+use crate::main;
 use crate::parser::Parser;
 
 pub(crate) fn function_node() {
-    let function = Parser::function(&mut Parser {
+    let code = fs::read_to_string("test2.wave").expect("Failed to read the file");
+    let mut parser = Parser {
         lexer: Lexer {
-            source: "",
+            source: &code,
             current: 0,
             line: 0,
         },
-        current_token: Default::default()
-        }, &mut AST {
-            nodes: vec![]
-        }
-    );
-    println!("{:?}", function);
+        current_token: Default::default(),
+    };
+
+    let mut ast = AST { nodes: vec![] };
+
+    parser.function(&mut ast);
+
+    // AST에 추가된 함수 노드를 출력
+    println!("{:#?}", ast);
 }
