3579c65fc484c4ce649d5daf3a068c9a162f992b
LunaStev <youngjae681@gmail.com>
2024-12-31 20:18:29 +0900

    & Delete the return keyword in the sector code

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -298,7 +298,7 @@ impl<'a> Lexer<'a> {
                         line: self.line,
                     }
                 } else {
-                    return Token {
+                    Token {
                         token_type: TokenType::BitwiseAnd,
                         lexeme: "&".to_string(),
                         line: self.line,
