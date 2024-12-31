58cbd9bae081fade510ab049372a2079fd666a38
LunaStev <youngjae681@gmail.com>
2024-12-31 19:52:29 +0900

    / Delete the return keyword in the sector code

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -181,7 +181,7 @@ impl<'a> Lexer<'a> {
                 }
             },
             '/' => {
-                return Token {
+                Token {
                     token_type: TokenType::DIV,
                     lexeme: "/".to_string(),
                     line: self.line,
