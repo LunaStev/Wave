9cbe7f5697191abc5e8277082f8cf530b7eb7875
LunaStev <youngjae681@gmail.com>
2025-01-01 21:33:36 +0900

    Add immutability detection with is_immutable flag in variable parsing

--- src/parser.rs
+++ src/parser.rs
@@ -124,6 +124,17 @@ impl<'a> Parser<'a> {
 
     fn variable(&mut self, ast: &mut AST) {
         // Processing 'var' tokens
+        let is_immutable = if self.current_token.token_type == TokenType::IMM {
+            self.advance();
+            true
+        } else {
+            false
+        };
+
+        if self.current_token.token_type != TokenType::VAR {
+            eprintln!("Error: Expected 'var', but got {:?}", self.current_token.token_type);
+            return
+        }
         self.advance(); // `var`
 
         // Processing variable names
