2e8e810255f8344774a3dbf053b667e432aea159
LunaStev <youngjae681@gmail.com>
2025-01-02 19:22:01 +0900

    Add FUN Parsing Code

--- src/parser.rs
+++ src/parser.rs
@@ -67,6 +67,9 @@ impl<'a> Parser<'a> {
 
     pub(crate) fn function(&mut self, ast: &mut AST) {
         eprintln!("Start parsing function...");
+        if self.current_token.token_type != TokenType::FUN {
+            panic!("Expected 'fun' keyword to start function, but got {:?}", self.current_token);
+        }
         self.advance(); // `fun`
 
         let name = if let TokenType::IDENTIFIER(name) = &self.current_token.token_type {
