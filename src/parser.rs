dc89330400156ade169a6d2c0d75ce74448a34ad
LunaStev <youngjae681@gmail.com>
2025-01-07 19:38:43 +0900

    Add Debug Message

--- src/parser.rs
+++ src/parser.rs
@@ -62,6 +62,7 @@ impl<'a> Parser<'a> {
     }
 
     fn advance(&mut self) {
+        eprintln!("Advancing from token: {:?}", self.current_token);
         self.current_token = self.lexer.next_token();
     }
 
