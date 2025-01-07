41bd1b8529f90ce5e56a84847e83d083c884d54a
LunaStev <youngjae681@gmail.com>
2025-01-07 19:45:28 +0900

    Add Debug Message

--- src/parser.rs
+++ src/parser.rs
@@ -87,7 +87,7 @@ impl<'a> Parser<'a> {
         self.advance(); // Consume function name
 
         if self.current_token.token_type != TokenType::LPAREN {
-            panic!("Expected '(' after function name");
+            panic!("Expected '(' after function name, but got {:?}", self.current_token);
         }
         self.advance();
 
