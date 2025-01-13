2e3c97066f86e0bccacd01a1a753ed87d73c3027
LunaStev <youngjae681@gmail.com>
2025-01-13 13:40:23 +0900

    Change the path

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod error;
 use std::fs;
 use lexer::{Lexer, Token};
 use crate::lexer::TokenType;
-use crate::parser::ast::create_function_ast;
+use crate::parser::create_function_ast;
 // use crate::node::function_node;
 
 fn format_tokens(tokens: &Vec<Token>) -> String {
