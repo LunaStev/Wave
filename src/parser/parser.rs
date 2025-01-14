32f3bc5261802be1885b0964d5cf6f0f8ec906e9
LunaStev <youngjae681@gmail.com>
2025-01-14 14:10:22 +0900

    'a additional

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -54,7 +54,7 @@ pub fn extract_parameters(tokens: &[Token]) -> Vec<ParameterNode> {
     params
 }
 
-pub fn body(tokens: &[Token]) -> Vec<ASTNode> {
+pub fn extract_body<'a>(tokens: &mut std::iter::Peekable<std::slice::Iter<'a, Token>>) -> Vec<ASTNode> {
     let mut body = vec![];
 
     while let Some(token) = tokens.next() {
