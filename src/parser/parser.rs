85d1cffedc9cc757ec749a493f92645228185a2c
LunaStev <youngjae681@gmail.com>
2025-01-16 23:36:44 +0900

    Update dependency code as readability improves

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -152,7 +152,7 @@ fn parse_var(tokens: &mut std::iter::Peekable<std::slice::Iter<'_, Token>>) -> O
                     if let Some(Token { token_type: TokenType::STRING(value), .. }) = tokens.next() {
                         return Some(ASTNode::Variable(VariableNode {
                             name: name.clone(),
-                            type_name: typename,
+                            type_name,
                             initial_value: Some(value.parse().unwrap()),
                         }));
                     }
