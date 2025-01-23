78ecf97afba12b38dfcc8af7bcf04b96dd334a28
LunaStev <youngjae681@gmail.com>
2025-01-23 16:48:40 +0900

    Existing code Peekable to Vec

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -19,7 +19,7 @@ pub fn param(parameter: String, param_type: String, initial_value: Option<String
     }
 }
 
-pub fn extract_parameters(tokens: &mut Peekable<Iter<Token>>) -> Vec<ParameterNode> {
+pub fn extract_parameters(tokens: &Vec<Token>) -> Vec<ParameterNode> {
     let mut params = vec![];
     let mut i = 0;
 
