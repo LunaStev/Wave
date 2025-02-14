0c1a74b5a2646ffbd930ebebd280b77db577ad00
LunaStev <youngjae681@gmail.com>
2025-02-14 16:42:54 +0900

    Call Printf and Create GEP to get i8* pointer

--- src/llvm_temporary/llvm_codegen.rs
+++ src/llvm_temporary/llvm_codegen.rs
@@ -19,16 +19,49 @@ pub fn generate_ir(ast: &ASTNode) -> String {
                             message
                         ));
 
-                        ir.push_str(&format!(
-                            "    call void @printf(i8* getelementptr inbounds ([{} x i8], [{} x i8]* {}, i32 0, i32 0))\n",
-                            message.len() + 1,
-                            message.len() + 1,
-                            string_label
-                        ));
-                    }
-                    _ => {}
-                }
+        let mut string_counter = 0;
+
+        for stmt in body {
+            if let ASTNode::Statement(StatementNode::Println(message)) = stmt {
+                // Generate unique global name
+                let global_name = format!("str_{}_{}", name, string_counter);
+                string_counter += 1;
+
+                // Create null-terminated string
+                let mut bytes = message.as_bytes().to_vec();
+                bytes.push(0);
+                let const_str = context.const_string(&bytes, false);
+
+                // Create global variable
+                let global = module.add_global(
+                    context.i8_type().array_type(bytes.len() as u32),
+                    None,
+                    &global_name,
+                );
+                global.set_initializer(&const_str);
+                global.set_linkage(Linkage::Private);
+                global.set_constant(true);
+
+                // Get printf function
+                let printf_type = context.i32_type().fn_type(
+                    &[context.i8_type().ptr_type(AddressSpace::default()).into()],
+                    true
+                );
+                let printf_func = module.add_function("printf", printf_type, None);
+
+                // Create GEP to get i8* pointer
+                let zero = context.i32_type().const_zero();
+                let indices = [zero, zero];
+                let gep = builder.build_gep(
+                    global.as_pointer_value(),
+                    &indices,
+                    "gep",
+                ).unwrap();
+
+                // Call printf
+                builder.build_call(printf_func, &[gep.into()], "printf_call");
             }
+        }
 
         // Add void return
         builder.build_return(None);
