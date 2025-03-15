5bd60f1b32227867d00c9ace1139b6314c26057f
LunaStev <youngjae681@gmail.com>
2025-03-15 19:51:09 +0900

    Handle 'else' branch

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -388,19 +388,12 @@ fn parse_if(tokens: &mut Peekable<Iter<Token>>) -> Option<ASTNode> {
                 }
             }
 
-            match tokens.peek() {
-                Some(token) if token.token_type == TokenType::Lbrace => {
-                    tokens.next(); // '{' Consumption
-                }
-                Some(_) => {
-                    println!("Error: Expected '{{' after 'else'");
-                    return None;
-                }
-                None => {
-                    println!("Error: Unexpected end of tokens. Expected '{{' after 'else'");
-                    return None;
-                }
+            // Handle 'else' branch
+            if tokens.peek()?.token_type != TokenType::Lbrace {
+                println!("Error: Expected '{{' after 'else'");
+                return None;
             }
+            tokens.next(); // Consume '{'
 
             else_block = Some(Box::new(parse_block(tokens)?));
             break;
