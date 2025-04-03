8ce28e7a50af2aed03073f11847475be3047a3d2
LunaStev <youngjae681@gmail.com>
2025-04-04 00:26:42 +0900

    error handling

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,60 @@
-// error.rs
+#[derive(Debug)]
+pub enum WaveErrorKind {
+    UnexpectedToken(String),
+    ExpectedToken(String),
+    UnexpectedChar(char),
+    SyntaxError(String),
+}
+
+#[derive(Debug)]
+pub struct WaveError {
+    pub kind: WaveErrorKind,
+    pub message: String,
+    pub file: String,
+    pub line: usize,
+    pub column: usize,
+    pub source: Option<String>,
+    pub label: Option<String>,
+}
+
+impl WaveError {
+    pub fn new(kind: WaveErrorKind, message: impl Into<String>, file: impl Into<String>, line: usize, column: usize) -> Self {
+        Self {
+            kind,
+            message: message.into(),
+            file: file.into(),
+            line,
+            column,
+            source: None,
+            label: None,
+        }
+    }
+
+    pub fn with_source(mut self, source: impl Into<String>) -> Self {
+        self.source = Some(source.into());
+        self
+    }
+
+    pub fn with_label(mut self, label: impl Into<String>) -> Self {
+        self.label = Some(label.into());
+        self
+    }
+
+    pub fn display(&self) {
+        eprintln!("error: {}", self.message);
+        eprintln!("  --> {}:{}:{}", self.file, self.line, self.column);
+        eprintln!("   |");
+
+        if let Some(source_line) = &self.source {
+            eprintln!("{:>3} | {}", self.line, source_line);
+            let arrow_line = format!("{:>3} | {:>width$}^", "", "", width = self.column - 1);
+            if let Some(label) = &self.label {
+                eprintln!("   | {} {}", &arrow_line[6..], label);
+            } else {
+                eprintln!("   | {}", &arrow_line[6..]);
+            }
+        } else {
+            eprintln!("   | (source unavailable)");
+        }
+    }
+}
