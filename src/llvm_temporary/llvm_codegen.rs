076cfa878750714407baa51cd0cc36e24ff84d61
LunaStev <youngjae681@gmail.com>
2025-04-06 21:47:53 +0900

    .

--- src/llvm_temporary/llvm_codegen.rs
+++ src/llvm_temporary/llvm_codegen.rs
@@ -167,7 +167,6 @@ fn generate_expression_ir<'ctx>(
 
             let call_site = builder.build_call(function, &compiled_args, "calltmp").unwrap();
 
-            // 리턴 타입이 있을 경우만 사용
             match function.get_type().get_return_type() {
                 Some(_) => call_site.try_as_basic_value().left().unwrap(),
                 None => context.i32_type().const_zero().as_basic_value_enum(),
