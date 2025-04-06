8a4bd20a1483fb0527cf8109d38a97dc5c0bea4c
LunaStev <youngjae681@gmail.com>
2025-04-06 19:01:15 +0900

    Use Value

--- src/llvm_temporary/llvm_codegen.rs
+++ src/llvm_temporary/llvm_codegen.rs
@@ -1,4 +1,4 @@
-use crate::parser::ast::{ASTNode, FunctionNode, StatementNode, Expression, VariableNode, Literal, Operator, WaveType};
+use crate::parser::ast::{ASTNode, FunctionNode, StatementNode, Expression, VariableNode, Literal, Operator, WaveType, Value};
 use inkwell::context::Context;
 use inkwell::module::Linkage;
 use inkwell::values::{PointerValue, FunctionValue, BasicValue, BasicValueEnum};
