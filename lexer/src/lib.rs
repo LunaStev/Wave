4795c4a55e8ce83ddf4a5bcc15340ae6f105cbe6
LunaStev <youngjae681@gmail.com>
2025-04-23 16:49:03 +0900

    use lexer and token

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,12 +1,4 @@
 pub mod lexer;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use lexer::*;
+pub use lexer::token::*;
