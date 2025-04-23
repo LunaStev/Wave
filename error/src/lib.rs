9d3fdc6d155aa4caab4d3289fcd341166514e022
LunaStev <youngjae681@gmail.com>
2025-04-23 16:39:47 +0900

    use error

--- error/src/lib.rs
+++ error/src/lib.rs
@@ -1,12 +1,3 @@
 mod error;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+use error::*;
