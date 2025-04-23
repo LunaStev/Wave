ee54a9737356fe08a6d5b4502a731b30de11bbca
LunaStev <youngjae681@gmail.com>
2025-04-23 16:49:22 +0900

    use parser

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,12 +1,3 @@
 pub mod parser;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use parser::*;
