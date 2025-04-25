af0f692c55620cff8740ed408dbb9565727b344c
LunaStev <youngjae681@gmail.com>
2025-04-25 12:39:13 +0900

    parser: import `Value` from AST module

--- llvm_temporary/src/llvm_temporary/llvm_codegen.rs
+++ llvm_temporary/src/llvm_temporary/llvm_codegen.rs
@@ -1,4 +1,4 @@
-use parser::ast::{ASTNode, FunctionNode, StatementNode, Expression, VariableNode, Literal, Operator, WaveType, Mutability};
+use parser::ast::{ASTNode, FunctionNode, StatementNode, Expression, VariableNode, Literal, Operator, WaveType, Mutability, Value};
 use inkwell::context::Context;
 use inkwell::module::Linkage;
 use inkwell::values::{PointerValue, FunctionValue, BasicValue, BasicValueEnum};
