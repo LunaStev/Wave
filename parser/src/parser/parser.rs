457511fc9122297ff6a0543d0bde4c14af306bee
LunaStev <youngjae681@gmail.com>
2025-05-03 22:48:02 +0900

    Add Number

--- parser/src/parser/parser.rs
+++ parser/src/parser/parser.rs
@@ -977,8 +977,14 @@ fn parse_asm_block(tokens: &mut Peekable<Iter<Token>>) -> Option<ASTNode> {
                     return None;
                 }
 
-                let var = match tokens.next()? {
-                    Token { token_type: TokenType::Identifier(s), .. } => s.clone(),
+                let token = tokens.next()?;
+                match &token.token_type {
+                    TokenType::Identifier(s) => {
+                        outputs.push((reg, s.clone()));
+                    }
+                    TokenType::Number(n) => {
+                        outputs.push((reg, n.to_string()));
+                    }
                     _ => {
                         println!("Expected identifier after out(...)");
                         return None;
