4d5f4b99de172e379d4f2e78189ba9ba2e015b37
LunaStev <youngjae681@gmail.com>
2025-05-03 18:49:09 +0900

    lifetime

--- src/version.rs
+++ src/version.rs
@@ -1,6 +1,6 @@
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-pub(crate) fn version() -> Box<str> {
+pub(crate) fn version() -> &'static str {
     let version = VERSION;
     version.into()
 }
