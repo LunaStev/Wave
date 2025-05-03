42b091ed4084bc76064ef669e288d200f4769af6
LunaStev <youngjae681@gmail.com>
2025-05-03 18:28:58 +0900

    Separate version as a function

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,7 @@ use std::path::Path;
 pub unsafe fn compile_and_run(path: &Path) {
     runner::run_wave_file(path);
 }
+
+pub fn version_wave() {
+    println!("{}", version::version().color("2,161,47"));
+}
